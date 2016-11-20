5da06c185d90c2342735d5b5a23098269945f3f3
kevinkim2540 <kevinkim2540@gmail.com>
2016-11-20 20:06:07 +0900

    Create Point.h

---
+++ Point.h
@@ -0,0 +1,22 @@
+#ifndef __POINT_H__
+#define __POINT_H__
+
+typedef struct _point
+{
+	int xpos;
+	int ypos;
+} Point;
+
+// Point 변수의 xpos, ypos 값 설정
+void SetPointPos(Point *ppos, int xpos, int ypos);
+
+// Point 변수의 xpos, ypos 정보 출력
+void ShowPointPos(Point *ppos);
+
+// 두 Point 변수의 비교
+// (x == x) = 1
+// (y == y) = 2
+// (x == x && y == y) = 0
+// (x != x && y != y) = -1
+int PointComp(Point *pos1, Point *pos2);
+#endif
